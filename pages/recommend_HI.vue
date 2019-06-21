9c439df96169b9f148534323dd176654b05760a0
Hiroki <hiroki_iwata0818@yahoo.co.jp>
2019-06-21 17:06:24 +0900

    font size

--- pages/recommend_HI.vue
+++ pages/recommend_HI.vue
@@ -26,7 +26,7 @@
       <div
       v-if="q_num==index+1">
         <div v-for="item_a in item"
-        font-size = "6"
+        font size = "6"
         :key="item_a.tag_id">
           <v-checkbox
           height = 2
