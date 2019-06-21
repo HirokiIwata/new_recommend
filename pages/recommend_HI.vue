a8034da8766e28d9b4736702e394ab5a49ae9ce1
Hiroki <hiroki_iwata0818@yahoo.co.jp>
2019-06-21 17:52:40 +0900

    change

--- pages/recommend_HI.vue
+++ pages/recommend_HI.vue
@@ -30,7 +30,7 @@
           <v-checkbox
           height = 2
           v-model="selected"
-          :size = 6
+          :size = 7
           :label="item_a.tag"
           :value="[item_a.tag,item_a.tag_id]"></v-checkbox>
         </div>
