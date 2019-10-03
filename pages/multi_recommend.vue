a346333dbb79d009dca39d950aecd73ba69d1aa4
Hiroki <hiroki_iwata0818@yahoo.co.jp>
2019-10-03 17:31:40 +0900

    modified

--- pages/multi_recommend.vue
+++ pages/multi_recommend.vue
@@ -334,82 +334,77 @@ export default {
       }
 
       function makeApiCall() {
-      var params = {
-        // The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '12ZftsGFxqwAw278FWBvIjkICemr1BnG_lhKjMjdXTBE',  // TODO: Update placeholder value.
+        var params = {
+            // The ID of the spreadsheet to retrieve data from.
+            spreadsheetId: '12ZftsGFxqwAw278FWBvIjkICemr1BnG_lhKjMjdXTBE',  // TODO: Update placeholder value.
 
-        // The A1 notation of the values to retrieve.
-        range: 'sheet1',  // TODO: Update placeholder value.
+            // The A1 notation of the values to retrieve.
+            range: 'sheet1',  // TODO: Update placeholder value.
 
-        // How values should be represented in the output.
-        // The default render option is ValueRenderOption.FORMATTED_VALUE.
-        valueRenderOption: '',  // TODO: Update placeholder value.
+            // How values should be represented in the output.
+            // The default render option is ValueRenderOption.FORMATTED_VALUE.
+            valueRenderOption: '',  // TODO: Update placeholder value.
 
-        // How dates, times, and durations should be represented in the output.
-        // This is ignored if value_render_option is
-        // FORMATTED_VALUE.
-        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-        dateTimeRenderOption: '',  // TODO: Update placeholder value.
-      };
+            // How dates, times, and durations should be represented in the output.
+            // This is ignored if value_render_option is
+            // FORMATTED_VALUE.
+            // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+            dateTimeRenderOption: '',  // TODO: Update placeholder value.
+        };
 
-      var request = gapi.client.sheets.spreadsheets.values.get(params);
-      request.then(function(response) {
-        // TODO: Change code below to process the `response` object:
-        console.log(response.result);
-      }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
-      });
-    }
-
-    function initClient() {
-      var API_KEY = 'AIzaSyA8ehMZYKnaFZTSNfWcfQRwE6MVXLXrjSc';  // TODO: Update placeholder with desired API key.
-
-      var CLIENT_ID = '178072879019-hup08tcbt51k0ciir87bpobl72bja4tu.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
-
-      // TODO: Authorize using one of the following scopes:
-      //   'https://www.googleapis.com/auth/drive'
-      //   'https://www.googleapis.com/auth/drive.file'
-      //   'https://www.googleapis.com/auth/drive.readonly'
-      //   'https://www.googleapis.com/auth/spreadsheets'
-      //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-
-      gapi.client.init({
-        'apiKey': API_KEY,
-        'clientId': CLIENT_ID,
-        'scope': SCOPE,
-        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-      }).then(function() {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      });
-    }
-
-    function handleClientLoad() {
-      gapi.load('client:auth2', initClient);
-    }
-
-    function updateSignInStatus(isSignedIn) {
-      if (isSignedIn) {
-        makeApiCall();
+        var request = gapi.client.sheets.spreadsheets.values.get(params);
+        request.then(function(response) {
+          // TODO: Change code below to process the `response` object:
+          console.log(response.result);
+          }, function(reason) {
+          console.error('error: ' + reason.result.error.message);
+        });
       }
-    }
 
-    function handleSignInClick(event) {
-      gapi.auth2.getAuthInstance().signIn();
-    }
+      function initClient() {
+        var API_KEY = 'AIzaSyA8ehMZYKnaFZTSNfWcfQRwE6MVXLXrjSc';  // TODO: Update placeholder with desired API key.
 
-    function handleSignOutClick(event) {
-      gapi.auth2.getAuthInstance().signOut();
-    }
+        var CLIENT_ID = '178072879019-hup08tcbt51k0ciir87bpobl72bja4tu.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
 
-    handleSignInClick();
+        // TODO: Authorize using one of the following scopes:
+        //   'https://www.googleapis.com/auth/drive'
+        //   'https://www.googleapis.com/auth/drive.file'
+        //   'https://www.googleapis.com/auth/drive.readonly'
+        //   'https://www.googleapis.com/auth/spreadsheets'
+        //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+        var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
-    </script>
-    <script async defer src="https://apis.google.com/js/api.js"
-      onload="this.onload=function(){};handleClientLoad()"
-      onreadystatechange="if (this.readyState === 'complete') this.onload()">
-    </script>
+        gapi.client.init({
+          'apiKey': API_KEY,
+          'clientId': CLIENT_ID,
+          'scope': SCOPE,
+          'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+        }).then(function() {
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+          updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        });
+      }
+
+      function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
+
+      function updateSignInStatus(isSignedIn) {
+        if (isSignedIn) {
+          makeApiCall();
+        }
+      }
+
+      function handleSignInClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+
+      function handleSignOutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+
+      handleSignInClick();
+
     },
 
     get_checkbox: function(event){
@@ -477,6 +472,11 @@ export default {
   }
 }
 </script>
+<script async defer src="https://apis.google.com/js/api.js"
+  onload="this.onload=function(){};handleClientLoad()"
+  onreadystatechange="if (this.readyState === 'complete') this.onload()">
+</script>
+
 
 <style>
 .query{
